496532925587fe441640220ff7f87d74054d811c
Your Name <you@example.com>
2017-07-30 00:40:47 +1200

    Second Task

--- AnimalID/AnimalID/AnimalID.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
+++ AnimalID/AnimalID/AnimalID.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -5,6 +5,7 @@ public class ApplicationRegistration {
 	public static void registerApplications ()
 	{
 				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("AnimalID.Droid.MainApplication, AnimalID.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md568209d4b737f554047e85ec0899c0dad.MainApplication.class, md568209d4b737f554047e85ec0899c0dad.MainApplication.__md_methods);
 		
 	}
 }
--- AnimalID/AnimalID/AnimalID/CustomVision.xaml.cs
+++ AnimalID/AnimalID/AnimalID/CustomVision.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -20,6 +22,33 @@ namespace AnimalID
         {
             InitializeComponent();
             
+        }
+        private async void LoadCamera(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+
+            MediaFile file = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+            {
+                PhotoSize = PhotoSize.Medium,
+                Directory = "Sample",
+                Name = $"{DateTime.UtcNow}.jpg"
+            });
+
+            if (file == null)
+                return;
+
+            image.Source = ImageSource.FromStream(() =>
+            {
+                return file.GetStream();
+            });
+
+            file.Dispose();
         }
     }
     
